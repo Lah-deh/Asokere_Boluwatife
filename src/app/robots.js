f95eb6c8ddeb0017c7872bb9b616cd1468ab78df
Lah-deh <successasokere@gmail.com>
2026-04-24 05:43:04 +0100

    changes

---
+++ src/app/robots.js
@@ -0,0 +1,10 @@
+s
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://asokere-boluwatife.vercel.app/sitemap.xml",
+  }
+}
---
+++ src/app/sitemap.js
@@ -0,0 +1,11 @@
+
+export default function sitemap() {
+  return [
+    {
+      url: "https://asokere-boluwatife.vercel.app",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+  ]
+}
